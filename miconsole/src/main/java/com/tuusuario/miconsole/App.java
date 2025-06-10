392c594c2929c85fe33fdc2ef2576ae908d5eaa8
LuketoDeus <66850029+lucascrvz@users.noreply.github.com>
2025-06-10 00:01:55 +0000

    implementacion clase persona

--- miconsole/src/main/java/com/tuusuario/miconsole/App.java
+++ miconsole/src/main/java/com/tuusuario/miconsole/App.java
@@ -1,13 +1,17 @@
 package com.tuusuario.miconsole;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.google.gson.Gson;
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+
+    public static void main(String[] args) {
+        String texto = " Hola Maven en la nube ";
+        System.out.println("¿Texto está en blanco?: " + StringUtils.isBlank(texto));
+        System.out.println("Texto sin espacios: '" + StringUtils.trim(texto) + "'");
+
+        Persona persona = new Persona("Lucas", 24);
+        String json = new Gson().toJson(persona);
+        System.out.println("Persona en JSON: " + json);
     }
 }
